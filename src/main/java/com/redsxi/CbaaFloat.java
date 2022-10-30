a24c0cdaef77cb115235ac79b767eaf21e648214
RedsXi <redsxi@qq.com>
2022-10-30 20:16:03 +0800

    Update CbaaFloat

--- src/main/java/com/redsxi/CbaaFloat.java
+++ src/main/java/com/redsxi/CbaaFloat.java
@@ -2,29 +2,22 @@ package com.redsxi;
 
 import com.redsxi.annotations.StaticByteArrayLength;
 
+import java.nio.ByteBuffer;
+
 @StaticByteArrayLength(4)
 public class CbaaFloat extends VariableCbaaObjBase<Float> {
     @Override
     public byte[] toByteArray() {
-        byte[] res = super.toByteArray();
-
-        int obj = Float.floatToIntBits(this.obj);
-
-        res[3] = (byte)((obj >> 24) & 0xFF);
-        res[2] = (byte)((obj >> 16) & 0xFF);
-        res[1] = (byte)((obj >> 8) & 0xFF);
-        res[0] = (byte)(obj & 0xFF);
-
-        return res;
+        ByteBuffer buffer = ByteBuffer.allocate(Float.BYTES);
+        buffer.putFloat(obj);
+        buffer.flip();
+        return buffer.array();
     }
 
     public CbaaFloat(byte[] src) {
-        super(src);
-        obj = Float.intBitsToFloat(
-                (src[0] & 0xFF) |
-                        ((src[1] & 0xFF) << 8) |
-                        ((src[2] & 0xFF) << 16) |
-                        ((src[3] & 0xFF) << 24)
-        );
+        ByteBuffer buffer = ByteBuffer.allocate(Float.BYTES);
+        buffer.put(src);
+        buffer.flip();
+        obj = buffer.getFloat();
     }
 }
