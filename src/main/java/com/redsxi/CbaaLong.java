10ae280a942b7ef0eeeb07470b6565b328d34f34
RedsXi <redsxi@qq.com>
2022-10-30 20:34:30 +0800

    Update CbaaLong

--- src/main/java/com/redsxi/CbaaLong.java
+++ src/main/java/com/redsxi/CbaaLong.java
@@ -1,4 +1,23 @@
 package com.redsxi;
 
-public class CbaaLong {
+import com.redsxi.annotations.StaticByteArrayLength;
+
+import java.nio.ByteBuffer;
+
+@StaticByteArrayLength(8)
+public class CbaaLong extends VariableCbaaObjBase<Long> {
+    @Override
+    public byte[] toByteArray() {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(obj);
+        buffer.flip();
+        return buffer.array();
+    }
+
+    public CbaaLong(byte[] src) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.put(src);
+        buffer.flip();
+        obj = buffer.getLong();
+    }
 }
